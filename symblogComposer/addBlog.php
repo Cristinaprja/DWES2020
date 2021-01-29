feb537daeb46835c85a31e9e2d4d259c4aff1648
Cristina Prieto Jalao <cristinaprja@gmail.com>
2021-01-29 10:59:40 +0100

    agregar blog funcionando

--- symblogComposer/addBlog.php
+++ symblogComposer/addBlog.php
@@ -1,6 +1,34 @@
 <?php
-include "datos/datos.php";
+// include "datos/datos.php";
+require_once "vendor/autoload.php";
+use App\Models\Blog;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'symblog',
+    'username'  => 'symblog',
+    'password'  => 'symblog',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+if(!empty($_POST)){
+    $blog = new Blog();
+    $blog->title = $_POST["title"];
+    $blog->blog = $_POST["description"];
+    $blog->tags = $_POST["tags"];
+    $blog->author = $_POST["author"];
+    $blog->save();
+}
 ?>
 <!DOCTYPE html>
 <html>
