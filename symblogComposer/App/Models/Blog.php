148660a749024d38ef28c64cc849706e3708f1b3
Cristina Prieto Jalao <cristinaprja@gmail.com>
2021-01-29 10:58:54 +0100

    clases terminadas

---
+++ symblogComposer/App/Models/Blog.php
@@ -0,0 +1,103 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model{
+
+    protected $table = "blog";
+    // private static $instancia;
+
+    // private $id;
+    // private $title;
+    // private $blog;
+    // private $imagen;
+    // private $author;
+    // private $tags;
+    // private $created;
+    // private $updated;
+
+    // public static function getInstancia() {
+    //     if (!isset(self::$instancia)) {
+    //         $miClase = __CLASS__;
+    //         self::$instancia = new $miClase;
+    //     }
+    //     return self::$instancia;
+    // }
+
+    // public function __clone() {
+    //     trigger_error('La clonación no es permitida.', E_USER_ERROR);
+    // }
+
+    // public function guardarBD(){    //HACERLO ASI
+    //     $this->query = "INSERT INTO blog (title, blog, imagen, author, tags) VALUES 
+    //         (:title, :blog, :imagen, :author, :tags)";
+    //     $this->parametros["title"] = $this->title;
+    //     $this->parametros["blog"] = $this->blog;
+    //     $this->parametros["imagen"] = $this->imagen;
+    //     $this->parametros["author"] = $this->author;
+    //     $this->parametros["tags"] = $this->tags;
+    //     // $this->parametros["created"] = $this->created;
+    //     // $this->parametros["updated"] = $this->updated;
+    //     $this->get_results_from_query();
+    //     $this->mensaje = 'Blog agregado exitosamente';
+    // }
+    // public function set(){
+
+    // }
+    // public function get(){
+
+    // }
+    // public function edit(){
+        
+    // }
+    // public function delete(){
+        
+    // }
+    // public function setTitle($title){
+    //     $this->title = $title;
+    // }
+    // public function getTitle(){
+    //     return $this->title;
+    // }
+    // public function setBlog($blog){
+    //     $this->blog = $blog;
+    // }
+    // public function getBlog()
+    // {
+    //     return $this->blog;
+    // }
+    // public function setImagen($imagen){
+    //     $this->imagen = $imagen;
+    // }
+    // public function getImagen()
+    // {
+    //     return $this->imagen;
+    // }
+    // public function setAuthor($author){
+    //     $this->author = $author;
+    // }
+    // public function getAuthor()
+    // {
+    //     return $this->author;
+    // }
+    // public function setTags($tags){
+    //     $this->tags = $tags;
+    // }
+    // public function getTags(){
+    //     return $this->tags;
+    // }
+    // public function setCreated($created){
+    //     $this->created = $created;
+    // }
+    // public function getCreated(){
+    //     return $this->created;
+    // }
+    // public function setUpdated($uptaded){
+    //     $this->uptaded = $uptaded;
+    // }
+    // public function getUpdated(){
+    //     return $this->updated;
+    // }
+    
+}
+?>
---
+++ symblogComposer/App/Models/Comment.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Models;
+// require_once('class/DBAbstractModel.php');
+
+// class Comment extends DBAbstractModel{
+class Comment{
+    private static $instancia;
+
+    private $id;
+    private $blog;
+    private $user;
+    private $comment;
+    private $approved;
+    private $created;
+    private $updated;
+
+    public static function getInstancia() {
+        if (!isset(self::$instancia)) {
+            $miClase = __CLASS__;
+            self::$instancia = new $miClase;
+        }
+        return self::$instancia;
+    }
+
+    public function __clone() {
+        trigger_error('La clonación no es permitida.', E_USER_ERROR);
+    }
+    public function guardarBD(){    //HACERLO ASI
+        $this->query = "INSERT INTO comment (blog, user, comment) VALUES 
+            ( :blog, :user, :comment)";
+        $this->parametros["blog"] = $this->blog;
+        $this->parametros["user"] = $this->user;
+        $this->parametros["comment"] = $this->comment;
+        // $this->parametros["created"] = $this->created;
+        // $this->parametros["updated"] = $this->updated;
+        $this->get_results_from_query();
+        $this->mensaje = 'comment agregado exitosamente';
+    }
+    public function set(){
+
+    }
+    public function get(){
+
+    }
+    public function edit(){
+        
+    }
+    public function delete(){
+        
+    }
+    public function setUser($user){
+        $this->user = $user;
+    }
+    public function getUser(){
+        return $this->user;
+    }
+    public function setComment($comment){
+        $this->comment = $comment;
+    }
+    public function getComment(){
+        return $this->comment;
+    }
+    public function setBlog($blog){
+        $this->blog = $blog;
+    }
+    public function getBlog(){
+        return $this->blog;
+    }
+    public function setApproved($approved){
+        $this->approved = $approved;
+    }
+    public function setCreated($created){
+        $this->created = $created;
+    }
+    public function getCreated(){
+        return $this->created;
+    }
+    public function setUpdated($uptaded){
+        $this->uptaded = $uptaded;
+    }
+}
+?>
